ce1852611841c74d5fabbe9d9e971236ad3cc084
Farid Hernández <farid.hdz.64@gmail.com>
2016-09-05 19:09:26 -0600

    commit plugins

---
+++ command_line.sql
@@ -0,0 +1,17 @@
+git branch
+git add --all
+git commit -am 'commit plugins'
+git pull origin shark
+git push origin shark
+
+git branch
+git add --all
+git commit -am 'commit plugins'
+git pull origin versionshark
+git push origin versionshark
+
+git branch
+git add --all
+git commit -am 'commit plugins'
+git pull origin farid
+git push origin farid
