53646049578270e94726f4f1e60c8d6bbdd41262
Daniel Farid Hernández Cortez <dfhernandez@salud.gob.sv>
2017-07-05 15:33:17 -0600

    Primer commit desde pc asignada

--- src/Minsal/SiblhBundle/Admin/BlhCurvaAdmin.php
+++ src/Minsal/SiblhBundle/Admin/BlhCurvaAdmin.php
@@ -73,16 +73,216 @@ class BlhCurvaAdmin extends MinsalSiblhBundleGeneralAdmin
     {
         $formMapper
             // ->add('id')
-            ->add('tiempo1')
-            ->add('tiempo2')
-            ->add('tiempo3')
-            ->add('valorCurva')
-            ->add('fechaCurva')
-            ->add('cantidadFrascos')
-            ->add('volumenPorFrasco')
-            ->add('horaInicioCurva')
+            ->add('tiempo1', null, array(
+                            'label' => '1er tiempo',
+                            'label_attr' => array('class' => 'label_form_sm'),
+                            'required' => false,
+                            'attr' => array(
+                                    'placeholder' => '1er tiempo...',
+                                    'class' => 'form-control input-sm',
+                                    // 'data-form-inline-group' => 'start',
+                                    // 'data-add-form-group-col-class' => 'col-lg-4 col-md-4 col-sm-4',
+
+                                    'data-add-input-addon' => 'true',
+                                    // 'data-add-input-addon-class' => 'primary-v4',
+                                    'data-add-input-addon-addon' => 'glyphicon glyphicon-heart-empty',
+
+                                    'data-fv-numeric' => 'true',
+                                    'data-fv-numeric-message' => 'El valor no es un número válido',
+                                    'data-fv-numeric-thousandsSeparator' => '',
+                                    'data-fv-numeric-decimalSeparator' => '.',
+
+                                    // 'min' => '2.27',
+                                    // 'max' => '226.80',
+                                    // 'data-fv-between-message' => 'Peso en Kg debe ser entre 2.27Kg y 226.80Kg',
+                            )
+            ))
+            ->add('tiempo2', null, array(
+                            'label' => '2do tiempo',
+                            'label_attr' => array('class' => 'label_form_sm'),
+                            'required' => false,
+                            'attr' => array(
+                                    'placeholder' => '2do tiempo...',
+                                    'class' => 'form-control input-sm',
+                                    // 'data-form-inline-group' => 'start',
+                                    // 'data-add-form-group-col-class' => 'col-lg-4 col-md-4 col-sm-4',
+
+                                    'data-add-input-addon' => 'true',
+                                    // 'data-add-input-addon-class' => 'primary-v4',
+                                    'data-add-input-addon-addon' => 'glyphicon glyphicon-heart-empty',
+
+                                    'data-fv-numeric' => 'true',
+                                    'data-fv-numeric-message' => 'El valor no es un número válido',
+                                    'data-fv-numeric-thousandsSeparator' => '',
+                                    'data-fv-numeric-decimalSeparator' => '.',
+
+                                    // 'min' => '2.27',
+                                    // 'max' => '226.80',
+                                    // 'data-fv-between-message' => 'Peso en Kg debe ser entre 2.27Kg y 226.80Kg',
+                            )
+            ))
+            ->add('tiempo3', null, array(
+                            'label' => '3er tiempo',
+                            'label_attr' => array('class' => 'label_form_sm'),
+                            'required' => false,
+                            'attr' => array(
+                                    'placeholder' => '3er tiempo...',
+                                    'class' => 'form-control input-sm',
+                                    // 'data-form-inline-group' => 'start',
+                                    // 'data-add-form-group-col-class' => 'col-lg-4 col-md-4 col-sm-4',
+
+                                    'data-add-input-addon' => 'true',
+                                    // 'data-add-input-addon-class' => 'primary-v4',
+                                    'data-add-input-addon-addon' => 'glyphicon glyphicon-heart-empty',
+
+                                    'data-fv-numeric' => 'true',
+                                    'data-fv-numeric-message' => 'El valor no es un número válido',
+                                    'data-fv-numeric-thousandsSeparator' => '',
+                                    'data-fv-numeric-decimalSeparator' => '.',
+
+                                    // 'min' => '2.27',
+                                    // 'max' => '226.80',
+                                    // 'data-fv-between-message' => 'Peso en Kg debe ser entre 2.27Kg y 226.80Kg',
+                            )
+            ))
+            ->add('valorCurva', null, array(
+                            'label' => 'Valor de curva',
+                            'label_attr' => array('class' => 'label_form_sm'),
+                            'required' => false,
+                            'attr' => array(
+                                    'placeholder' => 'valor curva...',
+                                    'class' => 'form-control input-sm',
+                                    // 'data-form-inline-group' => 'start',
+                                    // 'data-add-form-group-col-class' => 'col-lg-4 col-md-4 col-sm-4',
+
+                                    'data-add-input-addon' => 'true',
+                                    // 'data-add-input-addon-class' => 'primary-v4',
+                                    'data-add-input-addon-addon' => 'glyphicon glyphicon-heart-empty',
+
+                                    'data-fv-numeric' => 'true',
+                                    'data-fv-numeric-message' => 'El valor no es un número válido',
+                                    'data-fv-numeric-thousandsSeparator' => '',
+                                    'data-fv-numeric-decimalSeparator' => '.',
+
+                                    // 'min' => '2.27',
+                                    // 'max' => '226.80',
+                                    // 'data-fv-between-message' => 'Peso en Kg debe ser entre 2.27Kg y 226.80Kg',
+                            )
+            ))
+            ->add('fechaCurva', 'datetime', array(
+                            'label' => 'Fecha de registro de curva',
+                            'label_attr' => array('class' => 'label_form_sm'),
+                            'required' => false,
+                            'widget' => 'single_text',
+                            'format' => 'dd/MM/yyyy',
+                            'attr' => array(
+                                    // 'readonly' => 'readonly',
+                                    'placeholder' => 'DD/MM/YYYY',
+                                    'class' => 'form-control input-sm',
+                                    'data-input-transform' => 'datetimepicker',
+                                    'data-datetimepicker-type' => 'date',
+
+                                    'data-add-input-btn' => 'true',
+                                    'data-add-input-btn-class' => 'display-datetimepicker',
+                                    'data-add-input-btn-btn-class' => 'btn-sm btn-xray-awesome-blue background-opacity display-datetimepicker',
+                                    'data-add-input-btn-addon' => 'glyphicon glyphicon-time',
+
+                                    'data-fv-date' => 'true',
+                                    'data-fv-date-format' => 'DD/MM/YYYY',
+                                    'data-fv-date-message' => 'No es una fecha válida',
+                            )
+            ))
+            ->add('cantidadFrascos', null, array(
+                            'label' => 'Cantidad de frascos',
+                            'label_attr' => array('class' => 'label_form_sm'),
+                            'attr' => array(
+                                    'class' => 'form-control input-sm',
+                                    'placeholder' => 'cantidad de frascos...',
+
+                                    'data-add-input-addon' => 'true',
+                                    // 'data-add-input-addon-class' => 'primary-v4',
+                                    'data-add-input-addon-addon' => 'glyphicon glyphicon-tag',
+
+                                    'data-fv-integer' => 'true',
+                                    'data-fv-integer-message' => 'El valor no es un entero',
+
+                                    'min' => '0',
+                                    'max' => '32767',
+                                    'data-fv-between-message' => 'Número debe estar entre 0 y 32767',
+                            )
+            ))
+            ->add('volumenPorFrasco', null, array(
+                            'label' => 'Volumen por frasco',
+                            'label_attr' => array('class' => 'label_form_sm'),
+                            'required' => false,
+                            'attr' => array(
+                                    'placeholder' => 'volumen por frasco...',
+                                    'class' => 'form-control input-sm',
+                                    // 'data-form-inline-group' => 'start',
+                                    // 'data-add-form-group-col-class' => 'col-lg-4 col-md-4 col-sm-4',
+
+                                    'data-add-input-addon' => 'true',
+                                    // 'data-add-input-addon-class' => 'primary-v4',
+                                    'data-add-input-addon-addon' => 'glyphicon glyphicon-heart-empty',
+
+                                    'data-fv-numeric' => 'true',
+                                    'data-fv-numeric-message' => 'El valor no es un número válido',
+                                    'data-fv-numeric-thousandsSeparator' => '',
+                                    'data-fv-numeric-decimalSeparator' => '.',
+
+                                    // 'min' => '2.27',
+                                    // 'max' => '226.80',
+                                    // 'data-fv-between-message' => 'Peso en Kg debe ser entre 2.27Kg y 226.80Kg',
+                            )
+            ))
+            ->add('horaInicioCurva', 'datetime', array(
+                            'label' => 'Hora de inicio de curva',
+                            'label_attr' => array('class' => 'label_form_sm'),
+                            'required' => true,
+                            'widget' => 'single_text',
+                            'format' => 'HH:mm',
+                            'attr' => array(
+                                    'readonly' => 'readonly',
+                                    'placeholder' => 'HH:mm',
+                                    'class' => 'form-control input-sm',
+                                    'data-input-transform' => 'datetimepicker',
+                                    'data-datetimepicker-type' => 'time',
+
+                                    'data-add-input-btn' => 'true',
+                                    'data-add-input-btn-class' => 'display-datetimepicker',
+                                    'data-add-input-btn-btn-class' => 'btn-sm btn-xray-awesome-blue background-opacity display-datetimepicker',
+                                    'data-add-input-btn-addon' => 'glyphicon glyphicon-time',
+
+                                    'data-fv-date' => 'true',
+                                    'data-fv-date-format' => 'h:m A',
+                                    'data-fv-date-message' => 'No es una fecha válida',
+                            )
+            ))
             // ->add('usuario')
-            ->add('volumenTotal')
+            ->add('volumenTotal', null, array(
+                            'label' => 'Volumen total',
+                            'label_attr' => array('class' => 'label_form_sm'),
+                            'required' => false,
+                            'attr' => array(
+                                    'placeholder' => 'volumen total...',
+                                    'class' => 'form-control input-sm',
+                                    // 'data-form-inline-group' => 'start',
+                                    // 'data-add-form-group-col-class' => 'col-lg-4 col-md-4 col-sm-4',
+
+                                    'data-add-input-addon' => 'true',
+                                    // 'data-add-input-addon-class' => 'primary-v4',
+                                    'data-add-input-addon-addon' => 'glyphicon glyphicon-heart-empty',
+
+                                    'data-fv-numeric' => 'true',
+                                    'data-fv-numeric-message' => 'El valor no es un número válido',
+                                    'data-fv-numeric-thousandsSeparator' => '',
+                                    'data-fv-numeric-decimalSeparator' => '.',
+
+                                    // 'min' => '2.27',
+                                    // 'max' => '226.80',
+                                    // 'data-fv-between-message' => 'Peso en Kg debe ser entre 2.27Kg y 226.80Kg',
+                            )
+            ))
             // ->add('fechaHoraReg')
         ;
     }
--- src/Minsal/SiblhBundle/Entity/BlhCurva.php
+++ src/Minsal/SiblhBundle/Entity/BlhCurva.php
@@ -172,6 +172,7 @@ class BlhCurva implements EntityInterface
     public function __construct()
     {
         $this->fechaCurva = new \DateTime('now');
+        $this->horaInicioCurva = new \DateTime('now');
         $this->fechaHoraReg = new \DateTime('now');
     }
 
