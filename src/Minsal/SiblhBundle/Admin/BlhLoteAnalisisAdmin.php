f9e677495d04dd1879e12d821b62f765c379da55
Daniel Farid Hernández Cortez <dfhernandez@salud.gob.sv>
2017-08-07 12:30:58 -0600

    orm

--- src/Minsal/SiblhBundle/Admin/BlhLoteAnalisisAdmin.php
+++ src/Minsal/SiblhBundle/Admin/BlhLoteAnalisisAdmin.php
@@ -60,46 +60,6 @@ class BlhLoteAnalisisAdmin extends MinsalSiblhBundleGeneralAdmin
     protected function configureFormFields(FormMapper $formMapper)
     {
         $formMapper
-            ->with('Frascos recolectados no codificados en lote')
-                ->add('loteAnalisisFrascoRecolectado', 'entity', array(
-                                'label' => false,
-                                'label_attr' => array('class' => 'label_form_sm'),
-                                'required' => true,
-                                'expanded' => true,
-                                'multiple' => true,
-                                'class' => 'MinsalSiblhBundle:BlhFrascoRecolectado',
-                                // 'query_builder' => function(EntityRepository $er) use ($session_USER_LOCATION, $__XRAY_CLINICAL_SERVICE_ID__, $filter_modality_) {
-                                //                         return $er->createQueryBuilder('pryn')
-                                //                                     ->innerJoin('MinsalSiblhBundle:RyxCtlProyeccionEstablecimiento', 'prynhptl',
-                                //                                             \Doctrine\ORM\Query\Expr\Join::WITH,
-                                //                                             'pryn.id = prynhptl.idProyeccion')
-                                //                                     ->innerJoin('prynhptl.idAreaExamenEstab', 'mmxstd')
-                                //                                     ->innerJoin('mmxstd.idAreaServicioDiagnostico', 'mdld')
-                                //                                     ->where('mdld.idAtencion = :id_atn')
-                                //                                     ->setParameter('id_atn', $__XRAY_CLINICAL_SERVICE_ID__)  // --| 97 (Imagenología)
-                                //                                     ->andWhere('mmxstd.idEstablecimiento = :id_std')
-                                //                                     ->setParameter('id_std', $session_USER_LOCATION->getId())  // --| 97 (Hospital Local)
-                                //                                     ->andWhere('mmxstd.idAreaServicioDiagnostico = :id_mdld')
-                                //                                     ->setParameter('id_mdld', $filter_modality_)  // --| 97 (Modalidad filtro)
-                                //                                     ->andWhere('mmxstd.activo = TRUE')
-                                //                                     ->andWhere('prynhptl.habilitado = TRUE')
-                                //                                     ->orderBy('pryn.codigo')
-                                //                                     ->addOrderBy('pryn.nombre')
-                                //                                     ->distinct();
-                                //                     },
-                                'group_by' => 'idDonacion',
-                                'help' => '<span class="text-primary-v4">Complete el lote para iniciar el análisis</span>',
-                                'attr' => array(
-                                        'class' => /*'form-control input-sm'*/ 'list-inline formstyle-radio-list-inline input-sm'/* ul-splitted-list'*/,
-                                        'data-add-form-group-col-class' => 'col-lg-12 col-md-12 col-sm-12',
-                                        'data-sonata-select2-escape-markup' => 'true',
-                                        // 'style' => 'max-height: 500px; overflow-y: auto;'
-                                )
-                ))
-            ->end()
-        ;
-
-        $formMapper
             ->with('Nuevo lote')
                 // ->add('id')
                 ->add('codigoLoteAnalisis', null, array(
@@ -161,6 +121,41 @@ class BlhLoteAnalisisAdmin extends MinsalSiblhBundleGeneralAdmin
                 ))
                 // ->add('usuario')
                 // ->add('fechaHoraReg')
+                ->add('loteAnalisisFrascoRecolectado', 'entity', array(
+                                'label' => false,
+                                'label_attr' => array('class' => 'label_form_sm'),
+                                'required' => true,
+                                'expanded' => true,
+                                'multiple' => true,
+                                'class' => 'MinsalSiblhBundle:BlhFrascoRecolectado',
+                                // 'query_builder' => function(EntityRepository $er) use ($session_USER_LOCATION, $__XRAY_CLINICAL_SERVICE_ID__, $filter_modality_) {
+                                //                         return $er->createQueryBuilder('pryn')
+                                //                                     ->innerJoin('MinsalSiblhBundle:RyxCtlProyeccionEstablecimiento', 'prynhptl',
+                                //                                             \Doctrine\ORM\Query\Expr\Join::WITH,
+                                //                                             'pryn.id = prynhptl.idProyeccion')
+                                //                                     ->innerJoin('prynhptl.idAreaExamenEstab', 'mmxstd')
+                                //                                     ->innerJoin('mmxstd.idAreaServicioDiagnostico', 'mdld')
+                                //                                     ->where('mdld.idAtencion = :id_atn')
+                                //                                     ->setParameter('id_atn', $__XRAY_CLINICAL_SERVICE_ID__)  // --| 97 (Imagenología)
+                                //                                     ->andWhere('mmxstd.idEstablecimiento = :id_std')
+                                //                                     ->setParameter('id_std', $session_USER_LOCATION->getId())  // --| 97 (Hospital Local)
+                                //                                     ->andWhere('mmxstd.idAreaServicioDiagnostico = :id_mdld')
+                                //                                     ->setParameter('id_mdld', $filter_modality_)  // --| 97 (Modalidad filtro)
+                                //                                     ->andWhere('mmxstd.activo = TRUE')
+                                //                                     ->andWhere('prynhptl.habilitado = TRUE')
+                                //                                     ->orderBy('pryn.codigo')
+                                //                                     ->addOrderBy('pryn.nombre')
+                                //                                     ->distinct();
+                                //                     },
+                                'group_by' => 'idDonacion',
+                                'help' => '<span class="text-primary-v4">Complete el lote para iniciar el análisis</span>',
+                                'attr' => array(
+                                        'class' => /*'form-control input-sm'*/ 'list-inline formstyle-radio-list-inline input-sm'/* ul-splitted-list'*/,
+                                        'data-add-form-group-col-class' => 'col-lg-12 col-md-12 col-sm-12',
+                                        'data-sonata-select2-escape-markup' => 'true',
+                                        // 'style' => 'max-height: 500px; overflow-y: auto;'
+                                )
+                ))
             ->end()
         ;
     }
