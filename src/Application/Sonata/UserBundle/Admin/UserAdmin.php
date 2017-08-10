c46dadd19c15ff0c737f3dbd68625506b2227c36
Daniel Farid Hernández Cortez <dfhernandez@salud.gob.sv>
2017-08-10 11:38:59 -0600

    orm

--- src/Application/Sonata/UserBundle/Admin/UserAdmin.php
+++ src/Application/Sonata/UserBundle/Admin/UserAdmin.php
@@ -4,13 +4,37 @@ namespace Application\Sonata\UserBundle\Admin;
 
 // use Sonata\AdminBundle\Admin\Admin;
 use Sonata\UserBundle\Admin\Model\UserAdmin as BaseUserAdmin;
+// use Minsal\SiblhBundle\Admin\MinsalSiblhBundleGeneralAdmin;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Form\FormMapper;
 use Sonata\AdminBundle\Show\ShowMapper;
 
+use Doctrine\ORM\EntityRepository;
+use Sonata\AdminBundle\Validator\ErrorElement;
+use Sonata\AdminBundle\Route\RouteCollection;
+
 class UserAdmin extends BaseUserAdmin
 {
+    //////// << Bancos de Leche >>
+    const ___BLH_CLINICAL_SERVICE___    = '101';
+    ////////
+
+    // protected $baseRouteName    = 'siblh_usuario';
+    // protected $baseRoutePattern = 'blh/usuario';
+
+    protected function configureRoutes(RouteCollection $collection)
+    {
+        // $collection->remove('delete');
+        $collection->add('delete', 'borrar', [], [], ['expose' => true]);
+        $collection->add('create', 'crear', [], [], ['expose' => true]);
+        $collection->add('edit', 'editar', [], [], ['expose' => true]);
+        $collection->add('list', 'listar', [], [], ['expose' => true]);
+        $collection->add('show', 'consultar', [], [], ['expose' => true]);
+        $collection->add('generateTable', 'generar-tabla', [], [], ['expose' => true]);
+        $collection->add('generateData', 'generar-datos', [], [], ['expose' => true]);
+    }
+
     /**
      * @param DatagridMapper $datagridMapper
      */
@@ -158,7 +182,9 @@ class UserAdmin extends BaseUserAdmin
             ->add('gplusData')
             ->add('token')
             ->add('twoStepVerificationCode')
-            ->add('id')
+            ->add('idEstablecimiento')
+            ->add('idEmpleado')
+            // ->add('id')
         ;
     }
 
@@ -208,6 +234,37 @@ class UserAdmin extends BaseUserAdmin
             ->add('twoStepVerificationCode')
             ->add('id')
         ;
+    }
+
+    public function getFormTheme()
+    {
+        return array_merge(
+            parent::getFormTheme(),
+            array('MinsalSiblhBundle:Form:form_admin_fields.html.twig'),
+            array('MinsalSiblhBundle:Form:doctrine_orm_form_admin_fields.html.twig')
+       );
+    }
+    
+    public function getTemplate($name)
+    {
+        switch ($name)
+        {
+            case 'edit':
+                return 'MinsalSiblhBundle:CRUD:base_edit.html.twig';
+                break;
+            // case 'list':
+            //     return 'MinsalSiblhBundle:CRUD:base_list.html.twig';
+            //     break;
+            // case 'show':
+            //     return 'MinsalSiblhBundle:CRUD:base_show.html.twig';
+            //     break;
+            case 'delete':
+                return 'MinsalSiblhBundle:CRUD:delete.html.twig';
+                break;
+            default:
+                return parent::getTemplate($name);
+                break;
+        }
     }
 
 }
